6566437bfa8226507a220b6bd222208cfe6f4917
joaosantos86 <joaops41224@gmail.com>
2026-03-23 22:52:43 -0300

    feat: add validation for name, content and contacts in NewCampaign with tests

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,6 +1,7 @@
 package campaign
 
 import (
+	"errors"
 	"time"
 
 	"github.com/rs/xid"
@@ -18,8 +19,14 @@ type Campaign struct {
 	CreatedOn time.Time
 }
 
-func NewCampaign(name string, content string, emails []string) *Campaign {
-
+func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
+	if name == "" {
+		return nil, errors.New("Name is Required")
+	} else if content == "" {
+		return nil, errors.New("Content is Required")
+	} else if len(emails) == 0 {
+		return nil, errors.New("Emails is Required")
+	}
 	contacts := make([]Contact, 0, len(emails))
 	for _, email := range emails {
 		contacts = append(contacts, Contact{Email: email})
@@ -31,5 +38,5 @@ func NewCampaign(name string, content string, emails []string) *Campaign {
 		Content:   content,
 		CreatedOn: time.Now(),
 		Contacts:  contacts,
-	}
+	}, nil
 }
--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -8,8 +8,10 @@ import (
 )
 
 func newCampaign(t *testing.T) *Campaign {
+
 	t.Helper()
-	return NewCampaign("Campaign X", "body", []string{"email1@gmail.com", "email2@gmail.com"})
+	campaign, _ := NewCampaign("Campaign X", "body", []string{"email1@gmail.com", "email2@gmail.com"})
+	return campaign
 }
 
 func TestNewCampaign(t *testing.T) {
@@ -36,3 +38,24 @@ func TestNewCampaign_CreatedOnMusBeNow(t *testing.T) {
 	assert.Greater(campaign.CreatedOn, now)
 	// assert.NotZero(campaign.CreatedOn)
 }
+
+func TestNewCampaign_MustValidateName(t *testing.T) {
+	assert := assert.New(t)
+	_, err := NewCampaign("", "body", []string{"email1@gmail.com"})
+
+	assert.Equal("Name is Required", err.Error())
+}
+
+ func TestNewCampaign_MustValidateContent(t *testing.T) {
+ 	assert := assert.New(t)
+ 	_, err := NewCampaign("Campaign X", "", []string{"email1@gmail.com"})
+
+ 	assert.Equal("Content is Required", err.Error())
+ }
+
+ func TestNewCampaign_MustValidateContacts(t *testing.T) {
+ 	assert := assert.New(t)
+ 	_, err := NewCampaign("Campaign X", "body", []string{})
+
+ 	assert.Equal("Emails is Required", err.Error())
+ }
