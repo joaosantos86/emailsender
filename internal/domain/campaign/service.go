64d069bcd498d50aec21b51fea5046bc625cd2b3
joaosantos86 <joaops41224@gmail.com>
2026-03-24 23:18:04 -0300

    feat: create a mock to test the SaveCampaign

--- internal/domain/campaign/service.go
+++ internal/domain/campaign/service.go
@@ -1,11 +1,16 @@
 package campaign
 
-import contract "emailsender/internal/contracts"
+import (
+	contract "emailsender/internal/contracts"
+)
 
 type Service struct {
 	Repositery Repositery
 }
 
-func (s *Service) Create(newCampaign contract.NewCampaign) error {
-	return  nil
+func (s *Service) Create(newCampaign contract.NewCampaign) (string, error) {
+	campaign, _ := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+	s.Repositery.Save(campaign)
+	return campaign.ID, nil
+
 }
